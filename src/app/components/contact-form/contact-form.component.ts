557084263523e5ad00fa7af37d1bc798a3b207de
Daniel <137360936+danielmrz-dev@users.noreply.github.com>
2025-01-11 22:29:28 -0300

    Baixa icone de erro do figma e substitui no projeto

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { ButtonWithBorderBottomComponent } from "../button-with-border-bottom/button-with-border-bottom.component";
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [ButtonWithBorderBottomComponent, CommonModule],
+  imports: [CommonModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
 })
